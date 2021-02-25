61e0275f48542c112d92cbd6b3adf743fda1f2da
Richard Nai <richardnai6@gmail.com>
2021-02-25 17:13:46 +0100

    Adding initial files

---
+++ include/uavGS/ManeuverPlanner/WidgetManeuverEditor.h
@@ -0,0 +1,19 @@
+//
+// Created by seedship on 2/25/21.
+//
+
+#ifndef UAVGS_WIDGETMANEUVEREDITOR_H
+#define UAVGS_WIDGETMANEUVEREDITOR_H
+
+
+#include <QGraphicsScene>
+
+class WidgetManeuverEditor: public QGraphicsScene
+{
+	Q_OBJECT
+
+
+};
+
+
+#endif //UAVGS_WIDGETMANEUVEREDITOR_H
---
+++ src/ManeuverPlanner/WidgetManeuverEditor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by seedship on 2/25/21.
+//
+
+#include "uavGS/ManeuverPlanner/WidgetManeuverEditor.h"
